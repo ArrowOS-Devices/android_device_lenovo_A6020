0701f9047643c18b1532c0cd76713a52c8890f2e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-11-19 21:36:55 +0530

    A6020: init: Rework vendor init extension
     * Cleanup variant detection
     * Fix dalvik props for 3GB RAM variant
     * Override all props (LuK1337 <priv.luk@gmail.com>)
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>

--- init/init_A6020.cpp
+++ init/init_A6020.cpp
@@ -33,115 +33,111 @@
 #include <stdlib.h>
 
 #include <android-base/properties.h>
+#include <android-base/strings.h>
+
 #include "property_service.h"
 #include "vendor_init.h"
 
-using android::init::property_set;
-
 #define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
-#define CMDLINE_SIZE 1024
+using android::init::property_set;
 
-void property_override(char const prop[], char const value[])
+// copied from build/tools/releasetools/ota_from_target_files.py
+// but with "." at the end and empty entry
+std::vector<std::string> ro_product_props_default_source_order = {
+    "",
+    "vendor.",
+    "system.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
 {
-    prop_info *pi;
+    auto pi = (prop_info *) __system_property_find(prop);
 
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else
+    } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
 }
 
-void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+void override_device_props(char const model[], char const fingerprint[])
 {
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_build_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    for (const auto &source : ro_product_props_default_source_order)
+    {
+        set_ro_product_prop(source, "device", model);
+        set_ro_product_prop(source, "model", "Lenovo " + model);
+        set_ro_product_prop(source, "name", model);
+        set_ro_build_prop(source, "product", model);
+        set_ro_build_prop(source, "fingerprint", fingerprint);
+    }
 }
 
-void gsm_properties(bool msim);
-void set_model_config(bool plus);
-
-void vendor_load_properties()
+void set_model_config(bool fhd, int ram, bool dualsim = true)
 {
-    std::string platform;
-    std::string device;
-    char cmdlinebuff[CMDLINE_SIZE];
-    char board_id[32];
-    char panel_id[32];
-    FILE *fp;
+    if (ram == 3) {
+        // There's only one 3GB variant - and it has 1080p panel :)
+        property_set("ro.sf.lcd_density", "460");
 
-    if ((fp = fopen("/proc/cmdline", "r")) == NULL) {
-        printf("Failed to open /proc/cmdline");
-        return;
-    }
+        /* Dalvik properties for 1080p 3GB RAM
+         *
+         * https://github.com/CyanogenMod/android_frameworks_native/blob/cm-14.1/build/phone-xxhdpi-3072-dalvik-heap.mk
+         */
+        property_set("dalvik.vm.heapstartram", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "288m");
+        property_set("dalvik.vm.heapram", "768m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "512k");
+        property_set("dalvik.vm.heapmaxfree", "8m");
 
-    if (fgets(cmdlinebuff, CMDLINE_SIZE, fp) == NULL) {
-        return;
-    }
-    char *boardindex = strstr(cmdlinebuff, "board_id=");
-    if (boardindex != NULL) {
-        strncpy(board_id, strtok(boardindex + 9, ":"), 32);
-    }
-    char *panelindex = strstr(cmdlinebuff, "panel=");
-    if (panelindex != NULL) {
-        strncpy(panel_id, strtok(panelindex + 28, ":"), 32);
-    }
-    fclose(fp);
-
-    if (ISMATCH(board_id, "S82918B1")){
-        if (ISMATCH(panel_id, "ili9881c_720p_video") || ISMATCH(panel_id, "hx8394f_boe_720p_video")) {
-            property_override_dual("ro.build.product", "ro.vendor.build.product", "a6020a40");
-            property_override_dual("ro.product.device", "ro.vendor.product.device" , "a6020a40");
-            property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020a40/A6020a40:5.1.1/LMY47V/A6020a40_S007_161128_ROW:user/release-keys");
-            set_model_config(false); // 720p
-        } else { // panel_id = "otm1901a_tm_1080p_video" for A6020a46
-            property_override_dual("ro.build.product", "ro.vendor.build.product", "a6020a46");
-            property_override_dual("ro.product.device", "ro.vendor.product.device" , "a6020a46");
-            property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020a46/A6020a46:5.1.1/LMY47V/A6020a46_S042_160516_ROW:user/release-keys");
-            set_model_config(true); // 1080p
-        }
-        gsm_properties(true);
-    } else if (ISMATCH(board_id, "S82918E1")){
-        property_override_dual("ro.build.product", "ro.vendor.build.product", "a6020a41");
-        property_override_dual("ro.product.device", "ro.vendor.product.device" , "a6020a41");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020a41/A6020a41:5.1.1/LMY47V/A6020a41_S102_161123_ROW:user/release-keys");
-        set_model_config(false);
-        gsm_properties(false);
-    } else if (ISMATCH(board_id, "S82918F1")){
-        property_override_dual("ro.build.product", "ro.vendor.build.product", "A6020l36");
-        property_override_dual("ro.product.device", "ro.vendor.product.device" , "A6020l36");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020l36/A6020l36:5.1.1/LMY47V/A6020l36_S032_160401_LAS:user/release-keys");
-        set_model_config(true);
-        gsm_properties(true);
-    } else if (ISMATCH(board_id, "S82918G1")){
-        property_override_dual("ro.build.product", "ro.vendor.build.product", "a6020l37");
-        property_override_dual("ro.product.device", "ro.vendor.product.device" , "a6020l37");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020l37/A6020l37:5.1.1/LMY47V/A6020l37_S014_160402_LAS:user/release-keys");
-        set_model_config(true);
-        gsm_properties(false);
-    } else if (ISMATCH(board_id, "S82918H1")){
-        property_override_dual("ro.build.product", "ro.vendor.build.product", "a6020a46");
-        property_override_dual("ro.product.device", "ro.vendor.product.device" , "a6020a46");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020a46/A6020a46:5.1.1/LMY47V/A6020a46_S105_161124_ROW:user/release-keys");
-        set_model_config(true);
-        gsm_properties(true);
+        // Cached apps limit
+        property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "25");
     } else {
-        // Use A6020a40 as default - board_id = "S82918D1"
-        property_override_dual("ro.build.product", "ro.vendor.build.product", "a6020a40");
-        property_override_dual("ro.product.device", "ro.vendor.product.device" , "a6020a40");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/A6020a40/A6020a40:5.1.1/LMY47V/A6020a40_S102_161123_ROW:user/release-keys");
-        set_model_config(false);
-        gsm_properties(true);
+        if (fhd) {
+            property_set("ro.sf.lcd_density", "460");
+
+            /* Dalvik properties for 1080p 2GB RAM
+             *
+             * https://github.com/CyanogenMod/android_frameworks_native/blob/cm-14.1/build/phone-xxhdpi-2048-dalvik-heap.mk
+             */
+            property_set("dalvik.vm.heapstartram", "16m");
+            property_set("dalvik.vm.heapgrowthlimit", "192m");
+            property_set("dalvik.vm.heapram", "512m");
+            property_set("dalvik.vm.heaptargetutilization", "0.75");
+            property_set("dalvik.vm.heapminfree", "2m");
+            property_set("dalvik.vm.heapmaxfree", "8m");
+        } else {
+            property_set("ro.sf.lcd_density", "300");
+
+            /* Dalvik properties for 720p 2GB RAM
+             *
+             * https://github.com/CyanogenMod/android_frameworks_native/blob/cm-14.1/build/phone-xhdpi-2048-dalvik-heap.mk
+             */
+            property_set("dalvik.vm.heapstartram", "8m");
+            property_set("dalvik.vm.heapgrowthlimit", "192m");
+            property_set("dalvik.vm.heapram", "512m");
+            property_set("dalvik.vm.heaptargetutilization", "0.75");
+            property_set("dalvik.vm.heapminfree", "512k");
+            property_set("dalvik.vm.heapmaxfree", "8m");
+        }
+
+        // Cached apps limit
+        property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "17");
     }
 
-    // Init a dummy BT MAC address, will be overwritten later
-    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
-}
-
-void gsm_properties(bool msim)
-{
-    if (msim) {
+    if (dualsim) {
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "9,9");
     } else {
@@ -149,44 +145,62 @@ void gsm_properties(bool msim)
     }
 }
 
-void set_model_config(bool plus){
-    if (plus){
-        property_set("ro.sf.lcd_density", "460");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Vibe K5 Plus");
-        property_override_dual("ro.product.name", "ro.vendor.product.name", "Vibe K5 Plus");
+void vendor_load_properties()
+{
+    std::string platform;
+    std::string device;
+    char cmdlinebuff[128];
+    char board_id[32];
+    char panel_id[32];
+    FILE *fp;
 
-	/* Dalvik properties
-         *
-         * https://github.com/CyanogenMod/android_frameworks_native/blob/cm-14.1/build/phone-xxhdpi-2048-dalvik-heap.mk
-         */
-        property_set("dalvik.vm.heapstartsize", "16m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "512m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "2m");
-        property_set("dalvik.vm.heapmaxfree", "8m");
+    fp = fopen("/proc/cmdline", "r");
 
-        // Cached apps limit
-        property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "25");
+    // Get board and panel ID
+    char *boardindex = strstr(cmdlinebuff, "board_id=");
+    strncpy(board_id, strtok(boardindex + 9, ":"), 32);
+    char *panelindex = strstr(cmdlinebuff, "panel=");
+    strncpy(panel_id, strtok(panelindex + 28, ":"), 32);
 
+    fclose(fp);
+
+    //
+    // Variant detection
+    //
+    if (ISMATCH(board_id, "S82918B1")){
+        if (ISMATCH(panel_id, "ili9881c_720p_video") || ISMATCH(panel_id, "hx8394f_boe_720p_video")) {
+            // A6020a40 HW39 variant - SD616, 2GB, 720p
+            override_device_props("A6020a40", "Lenovo/A6020a40/A6020a40:5.1.1/LMY47V/A6020a40_S105_161128_ROW:user/release-keys");
+            set_model_config(false, 2);
+        } else {
+            // panel_id = "otm1901a_tm_1080p_video"
+            // A6020a46 Indonesian variant - SD616, 2GB, 1080p
+            override_device_props("A6020a46", "Lenovo/A6020a46/A6020a46:5.1.1/LMY47V/A6020a46_S042_160516_ROW:user/release-keys");
+            set_model_config(true, 2);
+        }
+    } else if (ISMATCH(board_id, "S82918E1")){
+        // A6020a41 - SD415, 2GB, 720p, single sim
+        override_device_props("A6020a41", "Lenovo/A6020a41/A6020a41:5.1.1/LMY47V/A6020a41_S_S028_160922_ROW:user/release-keys");
+        set_model_config(false, 2, false);
+    } else if (ISMATCH(board_id, "S82918F1")){
+        // A6020l36 - SD616, 2GB, 1080p
+        override_device_props("A6020l36", "Lenovo/A6020l36/A6020l36:5.1.1/LMY47V/A6020l36_S035_160622_LAS:user/release-keys");
+        set_model_config(true, 2);
+    } else if (ISMATCH(board_id, "S82918G1")){
+        // A6020l37 - SD616, 2GB, 1080p, single sim
+        override_device_props("A6020l37", "Lenovo/A6020l37/A6020l37:5.1.1/LMY47V/A6020l37_S024_160914_LAS:user/release-keys");
+        set_model_config(true, 2, false);
+    } else if (ISMATCH(board_id, "S82918H1")){
+        // A6020a46 Indian variant - SD616, 3GB, 1080p
+        override_device_props("A6020a46", "Lenovo/A6020a46/A6020a46:5.1.1/LMY47V/A6020a46_S105_161124_ROW:user/release-keys");
+        set_model_config(true, 3);
     } else {
-        property_set("ro.sf.lcd_density", "300");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Vibe K5");
-        property_override_dual("ro.product.name", "ro.vendor.product.name", "Vibe K5");
-
-        /* Dalvik properties
-         *
-         * https://github.com/CyanogenMod/android_frameworks_native/blob/cm-14.1/build/phone-xhdpi-2048-dalvik-heap.mk
-         */
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "512m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-
-        // Cached apps limit
-        property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "17");
-
+        // Use A6020a40 as default - SD415, 2GB, 720p
+        // board_id = "S82918D1"
+        override_device_props("A6020a40", "Lenovo/A6020a40/A6020a40:5.1.1/LMY47V/A6020a40_S102_161123_ROW:user/release-keys");
+        set_model_config(false, 2);
     }
+
+    // Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
 }

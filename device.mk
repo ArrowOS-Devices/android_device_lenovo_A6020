9d989eea057693a598be2b194c8b556bf4c72aa8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-12-17 19:00:04 +0530

    A6020: Binderize keymaster HAL

--- device.mk
+++ device.mk
@@ -350,13 +350,14 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
 
-# Keymaster HAL
+# Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-impl
 
 # Power
 PRODUCT_PACKAGES += \
